aafb4d812800616ec9b57ba09e9841a74a1de93c
Burak İskender <burak.iskendr@gmail.com>
2023-04-24 16:04:20 +0300

    :tada: initilaze project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const api_key = import.meta.env.VITE_API_KEY;
+
+function App() {
+  const [city, setCity] = useState();
+
+  useEffect(() => {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=Istanbul&aqi=no`)
+      .then((response) => response.json())
+      .then((data) => setCity(data.location));    
+  }, []);
+
+  return (
+    <div className='App'>
+      {city && (
+        <div>
+          <h1>{city.name}</h1>
+          <h2>{city.country}</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
